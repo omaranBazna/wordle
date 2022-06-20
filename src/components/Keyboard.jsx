7c284e2f4f8e11b5c9070847ac211f265a463870
omaranBazna <omran4d@gmail.com>
2022-06-20 17:44:24 -0400

    designing the keyboard

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,6 +1,25 @@
 import React from 'react'
 
 function Keyboard(){
-return <div>Keyboard</div>
+    const line1=["ض","ص","ث","ق","ف","غ","ع","ه","خ","ح"]
+    const line2=["ش","س","ي","ب","ل","ا","ت","ن","م","ك"]
+    const line3=["ر","ة","و"]
+return (
+
+<div className="keyboard">
+    
+<div className="keyboardRow1">
+  {line1.map((item,index)=><div className="btn" key={index}>{item}</div>)}
+</div>
+<div className="keyboardRow2">
+{line2.map((item,index)=><div className="btn" key={index}>{item}</div>)}
+</div>
+<div className="keyboardRow3">
+{line3.map((item,index)=><div className="btn" key={index}>{item}</div>)}
+</div>
+
+
+</div>
+)
 }
 export default Keyboard;
