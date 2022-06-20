28ce57e3e44ad6068a11d75dd5f137e929fd8bfb
omaranBazna <omran4d@gmail.com>
2022-06-20 17:09:01 -0400

    add the letter components

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,16 +1,57 @@
 import React from 'react'
 import {useState} from 'react'
 import Data from './Data'
+import Letter from './Letter'
 function Board(){
 const [board,setBoard]=useState(Data)
     return(
         <div className="board">
-           <div className="row"> </div>
-           <div className="row"> </div>
-           <div className="row"> </div>
-           <div className="row"> </div>
-           <div className="row"> </div>
-           <div className="row"> </div>
+           <div className="row">
+               <Letter LetterPos={0} AttemptVal={0}/>
+               <Letter LetterPos={1} AttemptVal={0}/>
+               <Letter LetterPos={2} AttemptVal={0}/>
+               <Letter LetterPos={3} AttemptVal={0}/>
+               <Letter LetterPos={4} AttemptVal={0}/>
+
+           </div>
+           <div className="row">
+           <Letter LetterPos={0} AttemptVal={1}/>
+           <Letter LetterPos={1} AttemptVal={1}/>
+           <Letter LetterPos={2} AttemptVal={1}/>
+           <Letter LetterPos={3} AttemptVal={1}/>
+           <Letter LetterPos={4} AttemptVal={1}/>
+            
+            </div>
+           <div className="row">
+           <Letter LetterPos={0} AttemptVal={2}/>
+           <Letter LetterPos={1} AttemptVal={2}/>
+           <Letter LetterPos={2} AttemptVal={2}/>
+           <Letter LetterPos={3} AttemptVal={2}/>
+           <Letter LetterPos={4} AttemptVal={2}/>
+            
+             </div>
+           <div className="row">
+           <Letter LetterPos={0} AttemptVal={3}/>
+           <Letter LetterPos={1} AttemptVal={3}/>
+           <Letter LetterPos={2} AttemptVal={3}/>
+           <Letter LetterPos={3} AttemptVal={3}/>
+           <Letter LetterPos={4} AttemptVal={3}/>
+             </div>
+           <div className="row">
+           <Letter LetterPos={0} AttemptVal={4}/>
+           <Letter LetterPos={1} AttemptVal={4}/>
+           <Letter LetterPos={2} AttemptVal={4}/>
+           <Letter LetterPos={3} AttemptVal={4}/>
+           <Letter LetterPos={4} AttemptVal={4}/>
+            
+             </div>
+           <div className="row">
+           <Letter LetterPos={0} AttemptVal={5}/>
+           <Letter LetterPos={1} AttemptVal={5}/>
+           <Letter LetterPos={2} AttemptVal={5}/>
+           <Letter LetterPos={3} AttemptVal={5}/>
+           <Letter LetterPos={4} AttemptVal={5}/>
+            </div>
         </div>
     )
 }
---
+++ src/components/Letter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+function Letter({AttemptVal,LetterPos}){
+    return (
+        <div className="letter">
+
+        </div>
+    )
+}
+export default Letter;
